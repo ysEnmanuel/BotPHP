cbedb45ee7e4204572e55cdf1fdc74c6cf161dd2
Enmanuel <65689762+ysEnmanuel@users.noreply.github.com>
2022-03-12 01:37:30 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,12 +7,14 @@
     $chat_id = $update["message"]["chat"]["id"];
     $username = $update["message"]["chat"]["username"];
     $message = $update["message"]["text"];
-
-    if($message == "/s"){
+    
+    if($message == "/start"){
+        send_message($chat_id, "Bienvenido @".$username." Este bot ha sido creado by @itsKyler666");
+    }
+    if($message == "/roll"){
         $number = rand(666,999);
         send_message($chat_id, $number);
     }
-
     if($message == "/time"){
         $masa = date("d/m/y h:i a", time());
         send_message($chat_id, $masa);
@@ -20,7 +22,8 @@
     
     function send_message($id, $msg){
         $text = urlencode($msg);
-        file_get_contents("https://api.telegram.org/bot5243109805:AAESxkazRdWTgCAiYY0A_9npQScCbYYUapY/sendMessage?chat_id=1054154613&text=$text");
+        file_get_contents("https://api.telegram.org/bot2007046980:AAF9McJweixsNSkIs8ZHCvjkax4ICWCkOjA/sendMessage?chat_id=$id&text=$text");
     }
+    
     
 ?>
