47f53155e80bf972ea5138dfa7d7d3a023f1c805
Enmanuel <65689762+ysEnmanuel@users.noreply.github.com>
2022-03-12 01:35:24 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,14 +7,18 @@
     $chat_id = $update["message"]["chat"]["id"];
     $username = $update["message"]["chat"]["username"];
     $message = $update["message"]["text"];
+    $from_chat_id = $update["message"]["chat"]["id"]
+    $message_id = $update["message"]["reply_to_message"]["id"]
     
-    if($message == "/start"){
-        send_message($chat_id, "Bienvenido @".$username." Este bot ha sido creado by @itsKyler666");
+    if($message == "/reenviar"){
+        send_message($chat_id, $from_chat_id, $message_id);
     }
-    if($message == "/roll"){
+
+    if($message == "/s"){
         $number = rand(666,999);
         send_message($chat_id, $number);
     }
+
     if($message == "/time"){
         $masa = date("d/m/y h:i a", time());
         send_message($chat_id, $masa);
@@ -22,8 +26,7 @@
     
     function send_message($id, $msg){
         $text = urlencode($msg);
-        file_get_contents("https://api.telegram.org/bot2007046980:AAF9McJweixsNSkIs8ZHCvjkax4ICWCkOjA/sendMessage?chat_id=$id&text=$text");
+        file_get_contents("https://api.telegram.org/bot5243109805:AAESxkazRdWTgCAiYY0A_9npQScCbYYUapY/sendMessage?chat_id=1054154613&text=$text");
     }
-    
     
 ?>
